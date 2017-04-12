00ad8d93cb55f11351dc097b1423cc98633c3a1d
Fulton Garcia <fultongarcia@gmail.com>
2017-04-11 22:19:41 -0400

    commented the functions

--- TechKnights/FirstViewController.swift
+++ TechKnights/FirstViewController.swift
@@ -17,25 +17,37 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
         itemTbl.reloadData()
     }
     
+    //Sets up the amount of rows to display
+    //we return the the length of the array
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemList.items.count
     }
     
+    //Creates the cells in the tableviews
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "Default Item")
+        
+        //sets the cell lable values to the name and the quantity
         cell.textLabel?.text = itemList.items[indexPath.row].name
         cell.detailTextLabel?.text = String(itemList.items[indexPath.row].quantity)
         
         return cell
     }
     
+    //enable the deletion of cells with a swipe
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        //Remove the item from the array that is being selected for deletion
         if(editingStyle == UITableViewCellEditingStyle.delete){
             itemList.items.remove(at: indexPath.row)
+            
+            //reload the date for the tableview
             itemTbl.reloadData()
         }
     }
     
+    //Reload the date in the table when the view appears again
+    //(when switching back from views)
     override func viewWillAppear(_ animated: Bool) {
         itemTbl.reloadData()
     }
--- TechKnights/SecondViewController.swift
+++ TechKnights/SecondViewController.swift
@@ -13,7 +13,10 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var nameTxt: UITextField!
     @IBOutlet weak var quantityTxt: UITextField!
     
+    //the function that will be fired off by the save button
     @IBAction func saveBtn(_ sender: AnyObject) {
+        
+        //if the name textfield is not empty add the name and quantity to the array
         if(nameTxt.text != ""){
             var num = Int(quantityTxt.text!)
             if(num == nil){
@@ -22,6 +25,7 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
             itemList.addItem(name: nameTxt.text!, quantity: num!)
             self.view.endEditing(true)
             
+            //empty the textfields after adding the new item
             nameTxt.text = ""
             quantityTxt.text = ""
         }
